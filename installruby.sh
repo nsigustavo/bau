139e481ccbde2bc742a0fa57b64b64b1697117e3
Gustavo Rezende <nsigustavo@gmail.com>
2010-07-19 20:50:08 -0300

    adicionado script de instalacao basica do ruby

---
+++ installruby.sh
@@ -0,0 +1,39 @@
+sudo apt-get -y install zlib1g zlib1g-dev
+sudo apt-get -y install ruby, irb, rails, rubygems
+sudo gem cucumber
+
+cd
+wget http://www.blue.sky.or.jp/atelier/ruby/ruby-zlib-0.6.0.tar.gz
+tar xvzf ruby-zlib-0.6.0.tar.gz
+cd ruby-zlib-0.6.0
+ruby extconf.rb
+make
+make install
+cd
+rm ruby-zlib-0.6.0.tar.gz
+rm -rf ruby-zlib-0.6.0
+
+gem install rails
+gem install cucumber
+
+
+echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"' >>  ~/.bashrc
+
+echo 'export PS1="\`ruby=\$(which ruby 1> /dev/null && ruby -v 2> /dev/null | grep -oP \"^.+?[0-9]+(\.[0-9]+)+\") && echo \"(\$ruby) \"\`$PS1"' >> ~/.bashrc
+
+source ~/.bashrc
+
+bash < <( curl http://rvm.beginrescueend.com/releases/rvm-install-head )
+
+rvm 1.8.7
+rvm 1.9.2
+
+rvm use 1.8.7
+
+gem install rails
+gem install cucumber
+
+
+
+
+
